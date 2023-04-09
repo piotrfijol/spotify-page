dbda808e5397f66981d31460d0beb52eb82ddca7
Piotr Fijol <piotr.fijol1@gmail.com>
2023-04-09 19:39:33 +0200

    Change function definitions with imports

--- src/pages/playlist-page/scripts/playlist-page.js
+++ src/pages/playlist-page/scripts/playlist-page.js
@@ -1,4 +1,5 @@
 import '../styles/playlist-page.scss'
+import { createSongItem } from './song-item';
 
 fetch("json/sampleData.json")
     .then((data) => data.json())
@@ -10,86 +11,3 @@ fetch("json/sampleData.json")
         })
 
     });
-
-function createSongMenu() {
-    let menuContainer = createElement('div', '', 'song__menu');
-    let menu = createElement('div', '', 'menu');
-    let menuDot = createElement('div', '', 'menu__dot');
-
-    appendChildren(
-        menu,
-        menuDot,
-        menuDot.cloneNode(),
-        menuDot.cloneNode()
-    );
-
-    menuContainer.appendChild(menu);
-
-    return menuContainer;
-}
-
-function createSongItem(song) {
-     
-    let songElement = createElement('div', '', 'song');
-    let songOrder   = createElement('p', song.order, 'song__name');
-    let songMain    = createElement('div', '', 'song__main');
-
-    appendChildren(
-        songMain,
-        createElement('img', '', '', {
-            src: song.album.img["112"]
-        }),
-        createSongInfoColumn(song.album.name, song.songName)
-    );
-    
-    let authorsColumn = createSongInfoColumn("Authors", song.authors.join(", "));
-    authorsColumn.classList.add("author");
-
-    let durationColumn = createSongInfoColumn("Time", song.duration);
-    durationColumn.classList.add("duration");
-
-    let menu = createSongMenu();
-
-    appendChildren(
-        songElement,
-        songOrder,
-        songMain,
-        authorsColumn,
-        durationColumn,
-        menu
-    );
-
-    return songElement
-
-} 
-
-function createSongInfoColumn(label, data) {
-    let column = createElement('div', '', 'song__info');
-    appendChildren(
-        column,
-        createElement('p', label, 'song__ label'),
-        createElement('p',  data, 'song__ data')
-    )
-
-    return column;
-}
-
-function createElement(type, textContent, classList, otherOptions = {}) {
-    let el = document.createElement(type);
-    el.textContent = textContent;
-    el.classList = classList;
-
-    if(Object.keys(otherOptions).length) {
-        for(let key in otherOptions) {
-            el.setAttribute(key, otherOptions[key]);
-        }
-    }
-
-    return el;
-}
-
-function appendChildren(parent, ...children) {
-    children.forEach((childElement) => {
-        parent.appendChild(childElement);
-    });
-}
